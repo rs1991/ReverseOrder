2169c8c51bb3a55478476934fdee41cb7acf7b8c
Rida Serroukh <rida.serroukh@gmail.com>
2022-03-05 22:23:33 +0000

    Add project files.

---
+++ ReverseOrder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReverseOrder 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String text1, reverseText = "";
+
+            Console.Write("Please enter some text: ");
+            text1 = Convert.ToString(Console.ReadLine());
+            for (int i = text1.Length - 1; i >= 0; i--)
+            {
+                reverseText = reverseText + text1[i];
+                
+            }
+            
+            Console.Write("Here is your text in reverse order: " + reverseText);
+            Console.Read();
+
+        }
+    }
+}
